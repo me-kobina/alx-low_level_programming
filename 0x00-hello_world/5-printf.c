26d9ea68b7631f3701f26c36e0933a7be6cbfd9b
Kobina <koomsonarnon@gmail.com>
2022-12-08 19:43:39 +0000

    exe

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - Entry point of the program
+* Description: using printf to print a string to stdout
+* Return: 0
+*/
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
